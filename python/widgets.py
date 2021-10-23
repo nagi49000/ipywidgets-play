e8fd54dee4861b47515bdaebb8cb6c004fc6b767
nagi49000 <nagi49000@gmail.com>
2021-10-23 22:00:21 +0100

    init commit

---
+++ python/widgets.py
@@ -0,0 +1,44 @@
+from ipywidgets import interact
+import ipywidgets as widgets
+from IPython.display import display
+
+
+class SimpleWidgets():
+    def __init__(self):
+        self._int_slider_widget = None
+        self._clicked_next_widget = None
+
+    def do_stuff_on_click(self, b):
+        if self._clicked_next_widget:
+            self._clicked_next_widget.close()
+        if self._int_slider_widget.value < 10:
+            @interact
+            def get_check_box():
+                x = widgets.Checkbox(value=False, description='Check me')
+                self._clicked_next_widget = x
+                return x
+        else:
+            @interact
+            def get_text():
+                x = widgets.Dropdown(options=['alpha', 'beta', 'gamma'], value='alpha', description='Text:')
+                self._clicked_next_widget = x
+                return x
+
+    def run_simple_widgets(self):
+
+        @interact
+        def get_int_slider():
+            x = widgets.IntSlider(min=0, max=30, step=1, value=10)
+            self._int_slider_widget = x
+            return x
+
+        button = widgets.Button(description="Click Me!")
+        display(button)
+        button.on_click(self.do_stuff_on_click)
+
+    @property
+    def last_value(self):
+        if self._clicked_next_widget:
+            return self._clicked_next_widget.value
+        else:
+            return None
